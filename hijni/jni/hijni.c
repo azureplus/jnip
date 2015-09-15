a400d14abfa205d9f1663cec1a035a869181de44
Qzure Qin <azureplus@icloud.com>
2015-09-15 18:48:54 +0800

    add jni

---
+++ hijni/jni/hijni.c
@@ -0,0 +1,12 @@
+//
+// Created by dev on 15-9-15.
+//
+#include "hijni.h"
+#include "jni.h"
+#include "stdio.h"
+
+JNIEXPORT void JNICALL Java_hijni_say
+(JNIEnv *env, jclass, jstring, jstring)
+{
+    Pri
+}
---
+++ hijni/src/hijni.java
@@ -0,0 +1,12 @@
+/**
+ * Created by dev on 15-9-15.
+ */
+public class hijni
+{
+  public static  native void say(String who,String something);
+
+  public static void main(String[] args)
+  {
+    say("my dev","tell me....");
+  }
+}
